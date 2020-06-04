1807ee22c21a7cc0e2b005c75b52f1415dc8119c
adam-wheatley <awheatley29560@gmail.com>
2020-06-04 01:57:33 +0100

    remove

--- src/styles/Global.js
+++ src/styles/Global.js
@@ -1,17 +1,6 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
-  @font-face {
-    font-family: 'TomatoSoup';
-    src: url('./fonts/TomatoSoup-Regular.otf');
-  }
-
-  @font-face {
-    font-family: 'Architectural';
-    src: url(../fonts/Architectural.ttf);
-  }
-
-
   html, body {
     font-family: 'Nunito', sans-serif;
     color: black;
